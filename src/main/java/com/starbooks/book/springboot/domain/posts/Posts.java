e6451b1502facc3292317fd4d02b1f8267c44cbf
chrisdohyung <chris.dohyung@gmail.com>
2021-04-17 17:24:24 +0900

    added .gitignore

--- src/main/java/com/starbooks/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/starbooks/book/springboot/domain/posts/Posts.java
@@ -1,14 +1,19 @@
 package com.starbooks.book.springboot.domain.posts;
 
+import com.starbooks.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,11 +26,16 @@ public class Posts {
     private String content;
 
     private String author;
+
     @Builder
     public Posts(String title,String content,String author){
         this.title=title;
         this.content=content;
         this.author=author;
+    }
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 
 
---
+++ src/test/java/com/starbooks/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.starbooks.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+@RunWith(SpringRunner.class)
+@SpringBootApplication
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+    @Test
+    public void callPostsRepository(){
+
+        String title ="테스트 게시글";
+        String content="테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("chris.dohyung@gmail.com")
+                .build());
+
+        List<Posts> postsList= postsRepository.findAll();
+
+        Posts posts=postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+    @Test
+    public void BaseTimeEntity_register() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2021, 4, 17, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+
+
+
+}
