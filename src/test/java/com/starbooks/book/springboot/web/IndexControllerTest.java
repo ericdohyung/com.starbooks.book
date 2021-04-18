137135f8958b9ff92a9216fb27c7a07db73f4592
chrisdohyung <chris.dohyung@gmail.com>
2021-04-18 16:14:16 +0900

    added .gitignore

---
+++ src/test/java/com/starbooks/book/springboot/web/IndexControllerTest.java
@@ -0,0 +1,26 @@
+package com.starbooks.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class IndexControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void mainpage_loading() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링부트로 시작하는 스타북스 웹 서비스");
+    }
+}
